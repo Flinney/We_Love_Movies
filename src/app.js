058657a871c707b9c30cd44778365fdffd340384
flinney <77643261+Flinney@users.noreply.github.com>
2021-10-17 19:35:49 -0500

    Add error handlers and boilerplate

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 if (process.env.USER) require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Error handlers
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
