dfa012902b85acbe90d7a3ed96ec819f55065d7f
flinney <77643261+Flinney@users.noreply.github.com>
2021-10-18 10:06:51 -0500

    init movies route

--- src/app.js
+++ src/app.js
@@ -6,10 +6,14 @@ const cors = require("cors");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
+const moviesRouter = require("./movies/movies.router");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/movies", moviesRouter);
 
 //Error handlers
 app.use(notFound);
